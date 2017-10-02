e8df30a5505a739e735d51c338223a705f4c83c6
apih97 <apih97@outlook.es>
2017-10-02 14:57:04 -0600

    Componer valdaciones
    Componer validaciones de Nombre, correo y correo

--- includes/registro.inc.php
+++ includes/registro.inc.php
@@ -18,21 +18,34 @@ if (isset($_POST['Registrar'])) {
 		exit();
 	}else {
 		//revisar si los caracteres puestos son validos
-		if (!preg_match("@ñ[a-zA-Z]*$", $nombre) || !preg_match("@ñ[a-zA-Z]*$", $apellido)) {
-			header("Location: ../registrar.php?registrar=caracinvalids");
-			exit();
-		}else {
-			//Revisar si correo es válido
-			if (!filter_var($correo), FILTER_VALIDATE_EMAIL) {
-				header("Location: ../registrar.php?registrar=malcorreo");
-				exit();	
+		if (!preg_match("/^[a-zA-Z ]*$/",$nombre) || !preg_match("/^[a-zA-Z ]*$/",$apellido)) {
+  				$nombreErr = "Solo letras";
+  				echo $nombreErr; 
+				header("Location: ../registrar.php?registrar=caracinvalids");
+				exit();
 			}else {
+			//Revisar si correo es válido
+			if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+  					$correoErr = "Correo invalido"; 
+					header("Location: ../registrar.php?registrar=malcorreo");
+					exit();	
+				}
+			else {
 				$sql = "SELECT * FROM usuario WHERE usuario_uid = '$Uid' ";
 				$resultado = mysqli_query($conn, $sql);
 				$checkresultado = mysqli_num_rows($resultado);
 
 				if ($checkresultado > 0) {
 					header("Location: ../registrar.php?registrar=uidtomado");
+					exit();	
+				}else {
+					//hashing (cifrar/distorcionar para que sea irreconcible) contraseña
+					$ContraHashed = password_hash($Contraseña, PASSWORD_DEFAULT);
+
+					//Insertar usuario en BD.
+					$sql = "INSERT INTO usuario (usuario_nombre, usuario_apellido, usuario_correo, usuario_uid, usuario_pass) VALUES ('$nombre', '$apellido', '$correo', '$Uid', '$ContraHashed');";
+					mysqli_query($conn, $sql);
+					header("Location: ../registrar.php?registrar=logro");
 					exit();	
 				}
 			}
